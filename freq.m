e9b311150dca46280f1026e4bfa4980e9fcc79e5
Jake <jacoba1@kpit.com>
2020-12-08 18:46:24 +0530

    removing freq.m from startup file

--- freq.m
+++ freq.m
@@ -1,10 +1,10 @@
 fre=500;
-sim('problem3.slx')
+sim('problem3.slx');
 fre=2000;
-sim('problem3.slx')
+sim('problem3.slx');
 fre=5000;
-sim('problem3.slx')
+sim('problem3.slx');
 fre=7500;
-sim('problem3.slx')
+sim('problem3.slx');
 fre=10000;
-sim('problem3.slx')
+sim('problem3.slx');
